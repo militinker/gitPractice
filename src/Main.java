597766a1af2eef14812603dc8ea7b07cc9a9fada
militinker <militinker@gmail.com>
2023-04-04 09:53:55 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        Country country=new Country();
+        country.country="Serbia";
+        country.state="vojvodina";
+    }
+}
